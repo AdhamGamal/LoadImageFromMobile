956d6616e43c3258c63953a17a7fec7b03e2e742
Adham Gamal <adhammohamadgamal@yahoo.com>
2017-09-26 23:26:52 +0200

    Initial commit

---
+++ app/src/main/java/com/amg/loadimagefrommobile/MainActivity.java
@@ -0,0 +1,34 @@
+package com.amg.loadimagefrommobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void load_image(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            Picasso.with(this).load(data.getData()).into((ImageView)findViewById(R.id.img));
+        }
+    }
+}
